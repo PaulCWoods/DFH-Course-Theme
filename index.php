e1ba5daa8fd8090029979d0c8168ffb77884c33e
PaulCWoods <pwoods5@icloud.com>
2026-08-21 09:49:15 +0100

    Initial commit

---
+++ index.php
@@ -0,0 +1,18 @@
+<?php
+/**
+ * The main template file
+ */
+get_header();
+?>
+<main class="site-main">
+    <?php
+    if ( have_posts() ) {
+        while ( have_posts() ) {
+            the_post();
+            the_content();
+        }
+    }
+    ?>
+</main>
+<?php
+get_footer();
